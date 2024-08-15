91a19235982be6029aa579a7f5ae8d8c6dbaa291
Hayat Tamboli <hayat.tamboli@gmail.com>
2024-08-16 02:21:10 +0530

    added vercel analytics

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,13 @@ import App from './App.vue'
 import router from './router'
 import Equal from '../node_modules/equal-vue'
 import Config from '../node_modules/equal-vue/dist/theme/light' // or light / dark theme
+import { inject } from '@vercel/analytics'
 
 const app = createApp(App)
 
 app.use(Equal, Config)
 app.use(createPinia())
 app.use(router)
+inject()
 
 app.mount('#app')
